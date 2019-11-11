d10b3137827b155248c53149ca669e49634a1409
Maja <rojasmaja5@gmail.com>
2019-11-11 12:24:07 -0800

    working on the footer and the site in wordpress

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -7,12 +7,55 @@
 
 ?>
 
-			</div><!-- #content -->
+
+
+
+
+
+
+
+
+
+			<div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
+
+			<section class="footer-info">
+		
+
+			<div class="contact-info">
+				<h3>Contact Info</h3>
+				
+				    <p><i class="fa fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
+					<p><i class="fa fa-phone"></i><a href="tel:5553434567891">778-456-7891</a></p>
+					<p>
+						<span><i class="fa fa-facebook-square"></i></span>
+						<span><i class="fa fa-twitter-square"></i></span>
+						<span><i class="fa fa-google-plus-square"></i></span>
+					</p>
+			</div>
+
+
+			<div class="business-hours">
+				<h3>Business Hours</h3>
+					<p class="mon-fri">Monday-Friday: 9am to 5pm</p>
+					<p class="saturday">Saturday: 10am to 2pm</p>
+					<p class="sunday">Sunday: Closed</p>
+			</div>
+
+			
+			
+
+
+			
+		</section>
+		<div class="container">Copyright © 2019 Inhabitent</div>
+
+		
+			
+            <!-- <div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				</div>.site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
