faadd021bbf72cb348fd0e2f28fe4a749786248e
Maja <rojasmaja5@gmail.com>
2019-11-27 14:11:32 -0800

    having to change the php on the content single to inhabitent

--- themes/inhabitent/js/script.js
+++ themes/inhabitent/js/script.js
@@ -1,19 +1,21 @@
 (function($) {
-  $(function() {
-    $('.site-header .icon-search').on('click', function(event) {
-      event.preventDefault();
-      if (!$('.site-header .icon-search').hasClass('search-opened')) {
-        $('.site-header .search-field')
-          .toggle('fast')
-          .focus();
-        $('.site-header .icon-search').addClass('search-opened');
-      }
-    });
-    $('.site-header .search-field').on('blur', function() {
-      if ($('.site-header .icon-search').hasClass('search-opened')) {
-        $('.site-header .search-field').toggle('fast');
-        $('.site-header .icon-search').removeClass('search-opened');
-      }
-    });
+  $('.search-submit').on('click', function(event) {
+    event.preventDefault();
+
+    if (!$('.search-field').hasClass('search-animation')) {
+      // $('.search-field').removeClass('search-hide');
+      $('.search-field').addClass('search-animation');
+      $('.search-field').focus();
+    } else {
+      $('.search-field').removeClass('search-animation');
+      // $('.search-field').addClass('search-hide');
+    }
+  });
+
+  $('.search-field').on('blur', function() {
+    // if ($('search-field').hasClass('search-opened')) {
+    // $('.search-field').toggle('fast');
+    $('.search-field').removeClass('search-animation');
+    // }
   });
 })(jQuery);
